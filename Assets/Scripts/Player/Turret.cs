2d5dffb01ee16b35910519550e77f3e1c30f9bfd
Noé <neo.laq.33@gmail.com>
2018-11-20 18:49:45 +0100

    ancient merge wip

--- Assets/Scripts/Player/Turret.cs
+++ Assets/Scripts/Player/Turret.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Turret : MonoBehaviour {
+public class Turret : LivingBeing {
 
     public float maxWanderRadius;
+    public List<Transform> targets;
     Vector3 destination;
     Vector2 randomPoint;
     bool isShooting;
+    bool canShoot;
 
     [Header("Shooting")]
     public float timeBetweenCols;
@@ -24,22 +26,24 @@ public class Turret : MonoBehaviour {
     public GameObject fireCollider;
     public ParticleSystem firePartSys;
     public Transform fireOrigin;
-    public Transform target;
+    Transform target;
 
-    private void Start()
+    public override void Start()
     {
+        base.Start();
+        targets = new List<Transform>();
         randomPoint = Random.insideUnitCircle * maxWanderRadius;
         destination = new Vector3(randomPoint.x, 0f, randomPoint.y);
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        //CheckShoot();
-        if(Input.GetKeyDown(KeyCode.Y))
+        CheckShoot();
+        if (Input.GetButtonDown("Fire1"))
         {
-		    Shoot (target);
+            if(canShoot) Shoot (target);
         }
-        if(Input.GetKeyUp(KeyCode.Y))
+        if (Input.GetButtonUp("Fire1"))
         {
             StopShooting();
         }
@@ -47,12 +51,12 @@ public class Turret : MonoBehaviour {
 
     void CheckShoot()
     {
-        if (isShooting)
+        if (isShooting )
         {
             if (fireTime >= timeBetweenCols)
             {
                 GameObject fireCol = Instantiate(fireCollider, fireOrigin.position, Quaternion.identity);
-                fireCol.GetComponent<Rigidbody>().velocity = playerRb.velocity + (shootTarget.position - fireOrigin.position).normalized * fireColSpeed;
+                fireCol.GetComponent<Rigidbody>().velocity = (shootTarget.position - fireOrigin.position).normalized * fireColSpeed;
                 fireTime = 0;
             }
             else
@@ -67,7 +71,7 @@ public class Turret : MonoBehaviour {
     }
 
     public void Shoot(Transform _target)
-    {
+    { 
         if (shootTarget == null)        //Transfer to spawn function
             shootTarget = _target;
 
@@ -78,6 +82,26 @@ public class Turret : MonoBehaviour {
         startShootingCor = StartCoroutine(IStartShooting());
     }
 
+    public void AddToTargetList (Transform targ)
+    {
+        if(targets.Count<=0)
+        {
+            targets.Add(targ);
+            target = targets[0];
+            canShoot = true;
+        }else targets.Add(targ);
+    }
+
+    public void RemoveAndChangeTarget(Transform targ)
+    {
+        targets.Remove(targ);
+        if(targets.Count > 0)
+        {
+        target = targets[0];
+        } else canShoot = false;
+
+    }
+
     public void StopShooting()
     {
         stopShootingCor = StartCoroutine(IStopShooting());
@@ -86,6 +110,7 @@ public class Turret : MonoBehaviour {
     IEnumerator IStartShooting()
     {
         yield return new WaitForSeconds(Random.Range(minShootDelay, maxShootDelay));
+        Debug.Log("shoot");
         firePartSys.Play();
         isShooting = true;
         fireTime = 0;

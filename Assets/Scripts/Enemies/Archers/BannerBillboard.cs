cfb61d92bc6d7e7849a43a2ff98556c98009608c
Noé <neo.laq.33@gmail.com>
2018-11-22 20:09:22 +0100

    added banner with feedback, and destruction of group on bannerman death

---
+++ Assets/Scripts/Enemies/Archers/BannerBillboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BannerBillboard : MonoBehaviour {
+
+    Transform cam;
+
+    private void Start()
+    {
+        cam = GameManager.Instance.mainCamera.transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 projectedPos = Vector3.ProjectOnPlane(cam.position, Vector3.up);
+        transform.LookAt(projectedPos);
+    }
+}

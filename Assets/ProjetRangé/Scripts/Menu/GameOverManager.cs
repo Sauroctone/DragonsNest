1526b7f4193f384de424a8bf1ac4793282f8613f
Louis <clouis466@gmail.com>
2019-02-12 18:10:06 +0100

    FinalGameOvver

---
+++ Assets/ProjetRangé/Scripts/Menu/GameOverManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverManager : MonoBehaviour {
+
+	public TextMananger score;
+	public TextMananger[] individualScore;
+
+
+	
+	// Use this for initialization
+	void Start () 
+	{
+		score.entry[0] = GameManager.Instance.scoreManager.score.ToString("000000");
+		
+		individualScore[0].entry[1] = GameManager.Instance.scoreManager.archerDeathCount.ToString("0000");
+		individualScore[1].entry[1] = GameManager.Instance.scoreManager.warlockDeathCount.ToString("0000");
+		individualScore[2].entry[1] = GameManager.Instance.scoreManager.ballistaDeathCount.ToString("0000");
+		
+	}
+	
+	
+}
